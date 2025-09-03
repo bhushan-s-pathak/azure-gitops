92ec22a9ccb8f00993516732a3916d47376fd63a
bhushan <bhushanpathakbackup@gmail.com>
2025-09-03 07:07:21 +0000

    Handle all exceptions gracefully

--- randon_joke.py
+++ randon_joke.py
@@ -16,7 +16,9 @@ def main():
             print(f"Failed to fetch data. Status code: {response.status_code}")
 
     except requests.exceptions.RequestException as e:
-        print(f"Error occurred: {e}")
+        print(f"Request related Error occurred: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
     main()
